82839199d76d62791fd5a2b5524242ef566f626c
ivelascom96 <ivanivan1996m@gmail.com>
2019-04-08 17:20:27 +0200

    Ya hemos hecho el cambio

--- Practica3Tema4Github/src/Ejercicio.java
+++ Practica3Tema4Github/src/Ejercicio.java
@@ -9,6 +9,7 @@ public class Ejercicio {
 						multiplo[x]=x+1;
 						System.out.println(multiplo[x]);
 				}
+					System.out.println("Holaaaaa");
 					for(int x=0; x<multiplo.length; x++){
 						if((multiplo[x]%10)==0)
 						System.out.println(multiplo[x]);
