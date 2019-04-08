975ec1bc71563d158b23c558f027a19738b3fa12
ivelascom96 <ivanivan1996m@gmail.com>
2019-04-08 16:19:37 +0200

    Hola

---
+++ Practica3Tema6/src/SerializableFile/Alumno.java
@@ -0,0 +1,5 @@
+package SerializableFile;
+
+public class Alumno {
+
+}
---
+++ Practica3Tema6/src/SerializableFile/SerializableFile.java
@@ -0,0 +1,10 @@
+package SerializableFile;
+
+public class SerializableFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
