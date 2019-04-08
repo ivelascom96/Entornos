a9998c98cd9903813cad9f270c9e1c9415068756
ivelascom96 <ivanivan1996m@gmail.com>
2019-04-08 16:52:03 +0200

    Ejercicio Entornos

---
+++ Practica3Tema4Github/src/Ejercicio.java
@@ -0,0 +1,23 @@
+
+public class Ejercicio {
+
+	public static void main(String[] args) {
+		
+		
+				int[] multiplo = new int [50];
+					for(int x=0; x<multiplo.length; x++){
+						multiplo[x]=x+1;
+						System.out.println(multiplo[x]);
+				}
+					for(int x=0; x<multiplo.length; x++){
+						if((multiplo[x]%10)==0)
+						System.out.println(multiplo[x]);
+					}
+					
+			}
+
+		
+
+	}
+
+
--- Practica3Tema6/src/SerializableFile/Alumno.java
+++ Practica3Tema6/src/SerializableFile/Alumno.java
@@ -1,5 +1,20 @@
 package SerializableFile;
 
 public class Alumno {
-
+	
+int vida;
+	
+	Alumno(){
+		vida=100;
+	}
+	
+	public int getVida(){
+		return vida;
+	}
+	
+	public void setVida(int vida) {
+		this.vida = vida;
+	}
 }
+
+
