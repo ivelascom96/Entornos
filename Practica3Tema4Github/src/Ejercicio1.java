9990f7cf3d769f3a70b9e1bde174823bfdb64f35
ivelascom96 <ivanivan1996m@gmail.com>
2019-04-10 21:19:37 +0200

    Este es el ejercicio 21

--- Practica3Tema4Github/src/Ejercicio1.java
+++ Practica3Tema4Github/src/Ejercicio1.java
@@ -6,7 +6,7 @@ public class Ejercicio1 {
 		int num1=16;
         int num2=5;
         
-        System.out.println("El resultado de la suma es "+(num1+num2));
+        System.out.println("El resultado de la suma es: "+(num1+num2));
         
         System.out.println("El resultado de la resta es "+(num1-num2));
  
